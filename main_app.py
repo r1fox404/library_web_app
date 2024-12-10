f015d647e66d8cf3181d16c57ae8775cd4e8b054
r1fox404 <r.ikt@yandex.ru>
2024-12-10 20:04:04 +0300

    Delete homepage

--- main_app.py
+++ main_app.py
@@ -7,8 +7,3 @@ fastapi_app = FastAPI(
     title="Some library`s web-application"
 )
 fastapi_app.include_router(router=api_router, prefix=settings.api_v1_prefix)
-
-
-@fastapi_app.get("/")
-async def homepage():
-    return {"message": "This is a home page :)"}

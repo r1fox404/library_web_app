0ddc4392902837fa46eae07391d48509d1cc1aea
r1fox404 <r.ikt@yandex.ru>
2024-12-10 16:08:06 +0300

    Type ignore for AsyncGenerator

--- core/db_models/connections.py
+++ core/db_models/connections.py
@@ -32,7 +32,7 @@ class DBConnection:
         return session
     
     
-    async def scoped_session(self) -> AsyncSession:
+    async def scoped_session(self) -> AsyncSession: #type: ignore
         session = self._get_scoped_session()
         yield session
         await session.close()

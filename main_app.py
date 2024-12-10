8030a73ab9e00bcb7a3bc2e22ac61531ad2718fe
r1fox404 <r.ikt@yandex.ru>
2024-12-10 19:37:12 +0300

    Add api_v1 main router

--- main_app.py
+++ main_app.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from core import settings
+from api import api_router
 
 
 fastapi_app = FastAPI(
     title="Some library`s web-application"
 )
+fastapi_app.include_router(router=api_router, prefix=settings.api_v1_prefix)
 
 
 @fastapi_app.get("/")

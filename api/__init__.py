a9588c5ce2eebd94f116f779fe05d15ca22603a0
r1fox404 <r.ikt@yandex.ru>
2024-12-10 20:04:55 +0300

    Upd routers import

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .v1.authors.views import router as authors_router
+from .v1.books.views import router as books_router
+from .v1.borrows.views import router as borrow_router
 from .v1.dependencies import SDepends
 from .v1.cruds import (
     Crud,
@@ -10,3 +12,5 @@ from .v1.cruds import (
 
 api_router = APIRouter()
 api_router.include_router(router=authors_router, prefix="/authors")
+api_router.include_router(router=books_router, prefix="/books")
+api_router.include_router(router=borrow_router, prefix="/borrows")

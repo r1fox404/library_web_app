512b1f7aaa0e30b41736aec5992039e5e2a27261
r1fox404 <r.ikt@yandex.ru>
2024-12-13 20:05:11 +0300

    Add router for Borrows

--- api/__init__.py
+++ api/__init__.py
@@ -3,12 +3,7 @@ from fastapi import APIRouter
 from .v1.authors.views import router as authors_router
 from .v1.books.views import router as books_router
 from .v1.borrows.views import router as borrow_router
-from .v1.dependencies import SDepends
-from .v1.cruds import (
-    Crud,
-    ModelType,
-    SchemaType
-)
+
 
 api_router = APIRouter()
 api_router.include_router(router=authors_router, prefix="/authors")

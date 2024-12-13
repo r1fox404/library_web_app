235f46855e46f5c39cce34f950ac10ee6ad82d46
r1fox404 <r.ikt@yandex.ru>
2024-12-13 20:06:31 +0300

    New Models for Borrows

--- core/db_models/Borrow.py
+++ core/db_models/Borrow.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .Base import OrmBase
 
@@ -6,7 +7,8 @@ from .Base import OrmBase
 class MBorrow(OrmBase):
     __tablename__ = "borrows"
     
-    book_id: Mapped[int]
+    status: Mapped[bool]
+    book_id: Mapped[int] = mapped_column(ForeignKey("books.id"))
     readers_name: Mapped[str]
     issue_date: Mapped[str]
     return_date: Mapped[str]
